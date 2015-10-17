4365a6cd751724b12791829a2686cfd73223d2bd
Will L. Fife <wfife@laika.com>
2015-10-17 03:05:25 -0700

    Initial commit of old rotor module for package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension
+
+rotor = Extension('rotor',
+	sources=['rotor/rotormodule.c']
+	depends=['rotor/rotormodule.c']
+	)
+
+setup(
+	name = 'rotormodule',
+	version = '1.0',
+	description = 'The Rotor Module',
+	ext_modules = [rotor]
+  	packages = ['rotormodule'],
+  	author = 'Lance Ellinghouse'
+  	url = 'https://github.com/sarlalian/rotormodule',
+  	keywords = ['encryption', 'logging', 'example'], # arbitrary keywords
+	classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
